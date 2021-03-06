ddad53aa9146abda2eaf2ca6aaeb5ded4d326a32
jsbsan <jusabejusabe@gmail.com>
2021-03-06 11:42:38 +0100

    pruebas

---
+++ data/Nuevacapaborrador_MultiRingBuffer_0.js
@@ -0,0 +1,1 @@
+var json_Nuevacapaborrador_MultiRingBuffer_0 = {"type":"FeatureCollection","name":"Nuevacapaborrador_MultiRingBuffer_0","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
